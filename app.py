feca8b82e89d41e57f31feea8d9ab2c37ca409cc
MEGADragon20 <fernandocormar@icloud.com>
2026-02-24 19:23:23 +0100

    first text

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/page/<page_name>')
-def page(page_name):
-    return render_template(f'{page_name}.html')   
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/page/<page_name>')
+    def page(page_name):
+        return render_template(f'{page_name}.html')   
+    return app
